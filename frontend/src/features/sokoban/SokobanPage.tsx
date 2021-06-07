42d9ddfee6b57ffef673b860ea96571362fd6cd3
Zach Youssef <zacharysamiyoussef@gmail.com>
2021-06-07 11:46:10 -0400

    Add dummy dropdowns / buttons for interacting with sokoban ai

--- frontend/src/features/sokoban/SokobanPage.tsx
+++ frontend/src/features/sokoban/SokobanPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Dropdown, Row, Col, ButtonGroup, Button, ButtonToolbar } from 'react-bootstrap';
 import { Stage, Layer } from 'react-konva';
 import SokobanGrid from './SokobanGrid';
 import SokobanSquare, { SquareType } from './SokobanSquare';
@@ -13,9 +13,33 @@ function Sokoban() {
     ]
     return (
         <Container>
-            <p>Insert options here</p>
+            <ButtonToolbar>
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        Level Select
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item> Test 1 </Dropdown.Item>
+                        <Dropdown.Item> Test 2 </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle variant="secondary">
+                        Agent Select
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item> AStar </Dropdown.Item>
+                        <Dropdown.Item> QAgent </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </ButtonToolbar>
+
             <SokobanGrid data={dummyLevel}/>
-            <p>Insert forward / back butons here</p>
+
+            <ButtonGroup>
+                <Button> Next Move </Button>
+                <Button variant="secondary"> Undo </Button>
+            </ButtonGroup>
         </Container>
     );
 }
