f3aa6caed1afb9c8cd4c488c59673a460286938b
Zach Youssef <zacharysamiyoussef@gmail.com>
2021-06-09 23:35:50 -0400

    Begin react-form-hook implementation

--- frontend/src/features/seamCarver/SeamCarverPage.tsx
+++ frontend/src/features/seamCarver/SeamCarverPage.tsx
@@ -1,78 +1,39 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { BaseSyntheticEvent, ChangeEvent, FormEvent, useState } from 'react';
 import { Button, Spinner } from 'react-bootstrap';
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormValues = {
+  file: FileList,
+  height: string,
+  width: string  
+};
 
 function SeamCarverPage() {
-    let [targetWidth, setTargetWidth] = useState<number>()
-    let [targetHeight, setTargetHeight] = useState<number>()
-
-    let [file, setFile] = useState<File>()
-    let [width, setWidth] = useState<number>(-1)
-    let [height, setHeight] = useState<number>(-1)
+    const { register, handleSubmit } = useForm();
     
-    let [loading, setLoading] = useState<Boolean>(false)
-    
-    function handleSetWidth(event: ChangeEvent<HTMLInputElement>) {
-        let w = parseInt(event.target.value)
-        if (w <= width || w >= 0) {
-            setTargetWidth(w)
-        }
-    }
-
-    function handleSetHeight(event: ChangeEvent<HTMLInputElement>) {
-        let h = parseInt(event.target.value)
-        if (h <= width || h >= 0) {
-            setTargetHeight(h)
-        }
+    let onSubmit: SubmitHandler<FormValues> = (data: FormValues, event? : BaseSyntheticEvent) => {
+        console.log(data)
+        console.log(event?.isDefaultPrevented())
     }
     
-    function handleFileSelect(event: ChangeEvent<HTMLInputElement>) {
-        let files = event.target.files
-        if (files === null) {
-            return
-        }
-        setFile(files[0])
-        
-        if (file === undefined){
-            console.error("Image failed to load :(")
-            return
-        }
-        
-        // Store the image's width and height for input validation
-        setLoading(true)
-        getImageDimensions(file!!).then(dim => {
-            setWidth(dim.width)
-            setTargetWidth(dim.width)
-            setHeight(dim.height)
-            setTargetHeight(dim.height)
-            setLoading(false)
-        })
-
-    }
-    
-    if (loading) {
-        return <Spinner animation="border"/>
-    }
-
     return (
-        <form action="http://localhost:8080/seamCarver/upload" method="post" encType="multipart/form-data">
+        <form 
+            action="http://localhost:8080/seamCarver/upload" 
+            method="post" 
+            encType="multipart/form-data"
+            onSubmit={handleSubmit(onSubmit)}
+        >
             <input 
                 type="file" 
-                name="file" 
-                onChange={handleFileSelect}
+                {...register("file", {required: true})}
             />
             <input 
                 type="number" 
-                name="width" 
-                disabled={file == undefined}
-                value={targetWidth}
-                onChange={handleSetWidth}
+                {...register("width", {required: true})}
             />
             <input 
                 type="number" 
-                name="height" 
-                disabled={file == undefined}
-                value={targetHeight}
-                onChange={handleSetHeight}
+                {...register("height", {required: true})}
             />
             <Button type="submit">Upload</Button>
         </form>
