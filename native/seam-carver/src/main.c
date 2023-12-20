78b7b549eb2e3934b8020be4f4454e3cc54caf7e
Zach Youssef <zacharysamiyoussef@gmail.com>
2023-12-20 11:22:19 -0500

    Compile stb image dependencies

--- native/seam-carver/src/main.c
+++ native/seam-carver/src/main.c
@@ -1,6 +1,3 @@
-#include <test.h>
-
 int main() {
-    test();
     return 0;
 }
---
+++ native/seam-carver/src/stb_impl.c
@@ -0,0 +1,7 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_LINEAR
+#define STBI_NO_HDR
+#include <stb_image.h>
