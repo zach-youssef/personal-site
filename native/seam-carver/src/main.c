590b0599b1bdf8c5f44195d292d1a01b1e8832b1
Zach Youssef <zacharysamiyoussef@gmail.com>
2023-12-20 11:32:42 -0500

    Read image file

--- native/seam-carver/src/main.c
+++ native/seam-carver/src/main.c
@@ -1,3 +1,20 @@
-int main() {
+#include <stdio.h>
+#include <stb_image.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("No file name specified");
+        return 1;
+    }
+
+    const char* filename = argv[1];
+    int width, height, channels;
+
+    stbi_uc* image = stbi_load(filename, &width, &height, &channels, STBI_rgb);
+
+    // TODO - actual work
+
+    stbi_image_free(image);
+
     return 0;
 }
