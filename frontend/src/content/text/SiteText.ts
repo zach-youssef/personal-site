9dd3817cfdd669d9274083e64b432b30ef72818c
zyoussef <zacharysamiyoussef@gmail.com>
2025-09-28 09:51:36 -0400

    Fix typo

--- frontend/src/content/text/SiteText.ts
+++ frontend/src/content/text/SiteText.ts
@@ -9,7 +9,7 @@ enum SiteText {
 const SiteTextContents: { [key in SiteText]: string } = {
     [SiteText.HeaderBar] : "Zach's Personal Website",
     [SiteText.AboutMeHeader] : "About Me",
-    [SiteText.AboutDesc] : "Software Engineer, Northeastern University Alum. \n I created this site as an interactive portfolio through wish to continue practicing skills I don't necessarily use in my day job.",
+    [SiteText.AboutDesc] : "Software Engineer, Northeastern University Alum. \n I created this site as an interactive portfolio through which to continue practicing skills I don't necessarily use in my day job.",
     [SiteText.FeaturedProjectHeader] : "Featured Project",
     [SiteText.ProjectHeader] : "University Projects"
 
