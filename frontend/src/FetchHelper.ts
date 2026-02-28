694f68e1c42880f4e2b80e671dfd934474d65157
zyoussef <zacharysamiyoussef@gmail.com>
2026-02-28 15:41:31 -0500

    Remove debug log

--- frontend/src/FetchHelper.ts
+++ frontend/src/FetchHelper.ts
@@ -1,9 +1,6 @@
 import ServerDomain from './ServerDomain';
 
 export async function fetchGraphQL(text: string, variables: object){
-    console.log(text);
-    console.log(variables);
-    
     const response = await fetch(`http://${ServerDomain}/graphql`, {
         method: 'POST',
         headers: {
